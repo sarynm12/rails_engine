3c50b616507bdb9e9325d977f70fbeb238d0b4dc
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-17 11:57:38 -0600

    update item model with associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,7 @@ class Item < ApplicationRecord
   validates_presence_of :name,
                         :description,
                         :unit_price
-  belongs_to :merchant, dependent: :destroy 
+  belongs_to :merchant, dependent: :destroy
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 end
