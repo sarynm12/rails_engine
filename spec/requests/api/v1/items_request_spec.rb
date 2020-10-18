86083d4737a2942512621c11e56aa5e0d4999a8a
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-18 09:54:53 -0600

    create item requests spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+RSpec.describe "Items API" do
+  it "sends a list of items" do
+    create(:merchant)
+    merchant = Merchant.first
+    create_list(:item, 3, merchant: merchant)
+    item = Item.last
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+  end
+end
