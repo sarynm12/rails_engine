d61d4c17de2f30b6c2d55bea7b61a3f4313a380d
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-18 10:12:39 -0600

    create spec for items index action

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -5,9 +5,24 @@ RSpec.describe "Items API" do
     create(:merchant)
     merchant = Merchant.first
     create_list(:item, 3, merchant: merchant)
-    item = Item.last
+
     get '/api/v1/items'
 
     expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items.count).to eq(3)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(Float)
+    end
   end
 end
