bbf5e2b9f200a17a204daf7e672afcee7bf560e5
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-18 12:15:51 -0600

    comment out create action

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -8,4 +8,14 @@ class Api::V1::Items::SearchController < ApplicationController
     render json: Item.find(params[:id])
   end
 
+  # def create
+  #   render json: Item.create(item_params)
+  # end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :created_at, :updated_at)
+  end
+
 end
