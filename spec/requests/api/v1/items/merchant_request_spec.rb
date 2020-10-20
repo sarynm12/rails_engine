8d5cc929703f66862d4f716ec64d946ca0e0bbb4
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-19 20:23:59 -0600

    create items folder under request specs

---
+++ spec/requests/api/v1/items/merchant_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Item/Merchant relationship" do
+  it "can find an items merchant" do
+    item = create(:item)
+
+    get "/api/v1/items/#{item.id}/merchant"
+
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(parsed.count).to eq(1)
+    expect(parsed[:data]).to have_key(:id)
+    expect(parsed[:data][:id]).to be_a(String)
+    expect(parsed[:data][:attributes]).to have_key(:name)
+    expect(parsed[:data][:attributes][:name]).to be_a(String)
+  end
+end
