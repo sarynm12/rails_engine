537489e6e8c75e0505303edc766b67a0f58469ff
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-19 18:56:51 -0600

    add index and show action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::MerchantsController < ApplicationController
 
+  def index
+    render json: MerchantSerializer.new(Merchant.all)
+  end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
   def create
     render json: MerchantSerializer.new(Merchant.create(merchant_params))
   end
