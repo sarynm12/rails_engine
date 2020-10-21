3d13897e563ccc3211445c9b7d78b942b0aad86e
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-21 13:54:36 -0600

    added index and show action to items find controller

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -8,11 +8,26 @@ class Api::V1::Items::FindController < ApplicationController
       elsif attribute == "description"
           item = Item.where("description ILIKE ?", "%#{item_params[:description]}%").first
       elsif attribute == "unit_price"
-          item = Item.where("item.#{attribute}" == "#{value}").first
+          item = Item.where(unit_price: "#{value}.to_f").first
       else attribute == "created_at" || "updated_at"
-        merchant = Item.where("item.#{attribute}.to_date" == "#{value.to_date}").first
+        item = Item.where("item.#{attribute}.to_date" == "#{value.to_date}").first
       end
     render json: ItemSerializer.new(item)
+  end
+
+  def index
+    attribute = params.keys.first
+    value = params[attribute]
+      if item_params.keys.first == "name"
+        items = Item.where("name ILIKE ?", "%#{item_params[:name]}%")
+      elsif attribute == "description"
+        items = Item.where("description ILIKE ?", "%#{item_params[:description]}%")
+      elsif attribute == "unit_price"
+        items = Item.where(unit_price: "#{value}.to_f")
+      else attribute == "created_at" || "updated_at"
+        items = Item.where("item.#{attribute}.to_date" == "#{value.to_date}")
+      end
+    render json: ItemSerializer.new(items)
   end
 
   private
