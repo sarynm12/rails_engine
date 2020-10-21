56ec7fc52b7288d791a6b9f61cee36645a975fe3
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-21 10:31:02 -0600

    check associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates_presence_of :name,
                         :description,
                         :unit_price
-  belongs_to :merchant, dependent: :destroy
+  belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
