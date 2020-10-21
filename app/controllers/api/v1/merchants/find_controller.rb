1a1ce49a60d49dca08778da82fb9a2e2c92a0d51
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-20 20:51:08 -0600

    add index action to find controller

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -11,6 +11,17 @@ class Api::V1::Merchants::FindController < ApplicationController
     render json: MerchantSerializer.new(merchant)
   end
 
+  def index
+    attribute = params.keys.first
+    value = params[attribute]
+      if merchant_params.keys.first == "name"
+        merchants = Merchant.where("name ILIKE ?", "%#{merchant_params[:name]}%")
+      else attribute == "created_at" || "updated_at"
+        merchants = Merchant.where("merchant.#{attribute}.to_date" == "#{value.to_date}")
+      end
+    render json: MerchantSerializer.new(merchants)
+  end
+
   private
 
   def merchant_params
