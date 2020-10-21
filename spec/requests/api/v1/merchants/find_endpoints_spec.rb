531b7449dcf9f013283eb3bfa6065e3e6c222dcc
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-20 20:54:51 -0600

    add index spec to merchants find spec

--- spec/requests/api/v1/merchants/find_endpoints_spec.rb
+++ spec/requests/api/v1/merchants/find_endpoints_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Merchant single finders' do
+RSpec.describe 'Merchant finders' do
   it 'can return a single merchant that matches the name parameters passed into a query' do
     merchant1 = create(:merchant)
     merchant2 = create(:merchant)
@@ -31,6 +31,23 @@ RSpec.describe 'Merchant single finders' do
 
     expect(merchant[:data][:attributes][:name]).to eq(merchant1.name)
     expect(merchant[:data][:attributes][:name]).to_not eq(merchant2.name)
+  end
+
+  it 'can return all records that match a set of query paramaters' do
+    merchant1 = create(:merchant, name: "Severus Snape")
+    merchant2 = create(:merchant, name: "Sev Slytherin")
+    merchant3 = create(:merchant, name: "Sly Dog")
+    merchant4 = create(:merchant, name: "Dog Man")
+
+    get "/api/v1/merchants/find_all?name=sly"
+
+    expect(response).to be_successful
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants[:data].count).to eq(2)
+    merchants[:data].each do |merchant|
+      expect(merchants[:data][0][:attributes][:name]).to eq(merchant2.name)
+      expect(merchants[:data][1][:attributes][:name]).to eq(merchant3.name)
+    end
   end
 
 end
