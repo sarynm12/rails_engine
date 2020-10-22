03e8599da38505d322a172e04058811d60f1643c
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-22 09:15:41 -0600

    add most items class method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,8 @@ class Merchant < ApplicationRecord
   def self.most_revenue(quantity)
     Merchant.select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue").joins(:invoice_items, :transactions).where(transactions: {result: "success"}, invoices: {status: "shipped"}).group(:id).order('revenue desc').limit(quantity)
   end
+
+  def self.most_items(quantity)
+    Merchant.select("merchants.*, SUM(invoice_items.quantity) AS sold").joins(:invoice_items, :transactions).where(transactions: {result: "success"}, invoices: {status: "shipped"}).group(:id).order('sold desc').limit(quantity)
+  end
 end
