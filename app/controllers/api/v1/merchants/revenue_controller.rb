73f4d3895184f6f8cafdb51736b8507081383bcd
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-22 15:30:59 -0600

    add show action while still figuring out revenue across dates

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -5,4 +5,8 @@ class Api::V1::Merchants::RevenueController < ApplicationController
     render json: MerchantSerializer.new(merchants)
   end
 
+  def show
+    render json: RevenueSerializer.new(Merchant.revenue_across_dates(params[:start_date], params[:end_date]))
+  end
+
 end
